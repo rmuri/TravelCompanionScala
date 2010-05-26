1860c90b458a627c60b8195ce5bfc36b513fabd8
Daniel Hobi <dhobi@.hsr.ch>
2010-05-26 10:54:42 +0200

    Excanvas

--- web/src/main/resources/toserve/gauge/gauge.js
+++ web/src/main/resources/toserve/gauge/gauge.js
@@ -1,73 +1,108 @@
 function Gauge() {
-	var _self = this;
-	var canvas;
-	var initvalue;
-	var finished = false;
+    var _self = this;
+    var canvas;
+    var initvalue;
+    var finished = false;
+    var counter = 0;
 
 
-	this.initialize = function(value,canvasid) {
-		canvas = canvasid;
-		initvalue = value;
-		objImg.onload  	= initCanvas;
-	}
+    this.initialize = function(value, canvasid) {
 
-	var initCanvas = function() {
-		var objCanvas = document.getElementById(canvas);
+        canvas = canvasid;
+        initvalue = value;
 
-		if(objCanvas != null) {
-			if(objCanvas.getContext){
-				draw(objCanvas,0);
-			}
-		}
-	}
+        if (objImg.complete == true && objImg2.complete == true) {
+            counter = 2;
+            initCanvas();
+        }
 
-	var calcRotation = function(value) {
-		///todo
-		var phi = (value/1.51)*Math.PI/50+(0.57);
-		if(phi < 6) { phi += 0.5; }
-		if(phi > 6) { phi = 6; }
+        objImg.onload = function() {
+            initCanvas();
+        }
+        objImg2.onload = function() {
+            initCanvas();
+        }
 
-		return phi;
-	}
+        objImg.onerror = function() {
+            initCanvas();
+        }
+        objImg2.onerror = function() {
+            initCanvas();
+        }
 
+        objImg.onabort = function() {
+            initCanvas();
+        }
+        objImg2.onabort = function() {
+            initCanvas();
+        }
 
-	var draw = function(objCanvas,i){
+    }
 
-	  var phi = calcRotation(i);
+    var initCanvas = function() {
+        counter++;
+        if (counter >= 2) {
+            var objCanvas = document.getElementById(canvas);
 
-	  // Kontext-Objekt
-	  var objContext = objCanvas.getContext("2d");
+            if (objCanvas != null) {
+                if (objCanvas.getContext) {
+                    draw(objCanvas, 0);
+                }
+            }
+        }
+    }
 
-	  objContext.clearRect(0, 0, 269, 269);  // Anzeigebereich leeren
-	  objContext.drawImage(objImg, 0, 0);    // Ziffernblatt zeichnen
+    var calcRotation = function(value) {
+        ///todo
+        var phi = (value / 1.51) * Math.PI / 50 + (0.57);
+        if (phi < 6) {
+            phi += 0.5;
+        }
+        if (phi > 6) {
+            phi = 6;
+        }
 
-	  objContext.font = "20pt Arial";
-	  objContext.textAlign = 'center';
-	  objContext.textBaseline = 'middle';
-
-	  objContext.fillText(i+"%", 145, 220);
-
-
-
-	  objContext.save();                     // Ausgangszustand speichern
-	  objContext.translate(135, 135);          // Koordinatensystem in Mittelpkt des Ziffernblatts verschieben
-
-	  objContext.save();
-
-	  objContext.rotate(phi);
-	              // Zeiger Ì?ber Mitte hinaus zeichnen
-	  objContext.drawImage(objImg2, -10, -35);
-
-	  objContext.restore();
-
-	  objContext.restore();
-
-	  if(i<initvalue) {
-		window.setTimeout(function() { draw(objCanvas,++i); },40);
-	  } else {
-			//alert(phi);
-	  }
+        return phi;
+    }
 
 
-	}
+    var draw = function(objCanvas, i) {
+        var phi = calcRotation(i);
+
+        // Kontext-Objekt
+        var objContext = objCanvas.getContext("2d");
+
+        objContext.clearRect(0, 0, 269, 269);  // Anzeigebereich leeren
+        objContext.drawImage(objImg, 0, 0);    // Ziffernblatt zeichnen
+
+        objContext.font = "20pt Arial";
+        objContext.textAlign = 'center';
+        objContext.textBaseline = 'middle';
+
+        objContext.fillText(i + "%", 145, 220);
+
+
+        objContext.save();                     // Ausgangszustand speichern
+        objContext.translate(135, 135);          // Koordinatensystem in Mittelpkt des Ziffernblatts verschieben
+
+        objContext.save();
+
+        objContext.rotate(phi);
+
+        objContext.drawImage(objImg2, -10, -35);
+
+        objContext.restore();
+
+        objContext.restore();
+
+        if (i < initvalue) {
+            window.setTimeout(function() {
+                draw(objCanvas, ++i);
+            }, 40);
+        } else {
+            //alert(phi);
+        }
+
+
+    }
 }
