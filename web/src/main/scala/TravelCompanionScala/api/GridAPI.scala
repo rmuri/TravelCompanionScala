d1f56734d086c95932ff35c2775ae0f3c2053f19
Paul Bernet <paul.bernet@crealogix.com>
2010-06-09 10:19:24 +0200

    sorting for jqgrid added

--- web/src/main/scala/TravelCompanionScala/api/GridAPI.scala
+++ web/src/main/scala/TravelCompanionScala/api/GridAPI.scala
@@ -16,28 +16,64 @@ import net.liftweb.util.Helpers._
 import net.liftweb.http.{SessionVar, RequestVar, SHtml, S}
 import net.liftweb.http.rest.{XmlSelect, JsonSelect, RestHelper}
 import net.liftweb.json.Xml
-import xml.{Elem, Text}
+import xml.{Elem, Text, Node}
+import collection.mutable.Buffer
+
 object tourVar extends RequestVar[Tour](new Tour())
 
 object GridAPI extends RestHelper {
 
-  var tourData = Model.findAll[Tour]("findAllTours")flatMap(e => e.toGrid)
-  val tourSize = Model.findAll[Tour]("findAllTours").size
+  //This var is needed  for the construction of the xml
+  var tourData = Buffer[Node] ()
+
+  //a more performant solution with direct sql query
+  val resultOption: Option[java.lang.Long] = Model.createNativeQuery("select count(*) from tours").findOne
+  val result = resultOption.getOrElse(new java.lang.Long(100))
+  val tourSize = toInt(result)
+
   var page = 1;
   var rows = 10;
 
-  //var foo = SHtml.link("view", () => tourVar(e), Text("Foobar"))
 
   def getData = {
      for {
       Spage <- S.param("page") ?~ "page parameter missing" ~> 400
       Srows <- S.param("rows") ?~ "row parameter missing" ~> 400
+      //the sort order
+      Ssord <- S.param("sord") ?~ "row parameter missing" ~> 400
+      //which column: ID, Name, Description
+      Ssidx <- S.param("sidx") ?~ "row parameter missing" ~> 400
+
     } yield {
       page = Spage.toInt -1
       rows = Srows.toInt
-      val query = Model.createNamedQuery[Tour]("findAllTours")
-              .setFirstResult(page*rows).setMaxResults(rows)
-      var tourData2 = query.findAll
+
+      //Not posssible to create typesafe query - because only in Java with Java Entity-Types
+      //see http://www.ibm.com/developerworks/java/library/j-typesafejpa
+       var queryString = "SELECT t from Tour t order by"
+
+       Ssidx match {
+         case "ID" =>  queryString = queryString.concat(" t.id")
+         case "Name" =>  queryString = queryString.concat(" t.name")
+         case "Description" => queryString = queryString.concat(" t.description")
+         case _  => queryString = queryString.concat(" t.id")
+       }
+
+       Ssord match {
+         case "asc" => queryString = queryString.concat(" ASC")
+         case "desc" => queryString = queryString.concat(" DESC")
+         case _  =>  queryString = queryString.concat(" ASC")
+       }
+
+
+      val customQuery = Model.createQuery[Tour](queryString)
+      //Not all DBs support FETCH, FIRST, JPA might behave strange when changing the DB
+      //see http://troels.arvin.dk/db/rdbms/#select-limit
+      customQuery.setFirstResult(page*rows)
+      customQuery.setMaxResults(rows)
+
+
+      var tourData2 = customQuery.getResultList()
       tourData = tourData2.flatMap(tour => bind("tour", tour.toGrid, "name" -> PCData(SHtml.link("view", () => tourVarFromAPI(tour), Text(tour.name)))))
     }
   }
